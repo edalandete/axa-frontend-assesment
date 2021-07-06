787b990ddab4dcff33f95835c5dc39c3de36e84f
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-07-06 12:11:33 +0200

    Create filter by hair color

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { getOrcsByHairColor } from '../selectors/orcsSelector';
 import actionTypes from './actionTypes';
 
 const url = process.env.REACT_APP_ORCS_API;
 
-export default function loadOrcs() {
+export function loadOrcs() {
   return async (dispatch) => {
     try {
       const { data } = await axios(url);
@@ -14,6 +15,25 @@ export default function loadOrcs() {
     } catch (error) {
       dispatch({
         type: actionTypes.LOAD_ORCS_ERROR
+      });
+    }
+  };
+}
+
+export function filterOrcs(orcs, searchValue) {
+  return async (dispatch) => {
+    try {
+      const filteredOrcs = searchValue
+        ? getOrcsByHairColor(orcs, searchValue)
+        : orcs;
+
+      dispatch({
+        type: actionTypes.FILTER_ORCS,
+        filteredOrcs
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.FILTER_ORCS_ERROR
       });
     }
   };
